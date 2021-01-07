dbe61db4651ddf5c860477fdf65e9c77f001313c
eduardonk9999 <thiago.novak7@gmail.com>
2021-01-07 12:29:53 -0300

    ADD, class, arrow function, metodos de array

---
+++ Deconstruction/app.js
@@ -0,0 +1,10 @@
+let [x, y] = ['a', 'b'];
+
+// Executar o código anterior é o mesmo que fazer o seguinte:
+let x = 'a';
+let y = 'b';
+
+// Property shorthand, que é outra maneira de executar a desestruturação de objetos
+let [x, y] = ['a', 'b'];
+let obj = {x, y};
+
