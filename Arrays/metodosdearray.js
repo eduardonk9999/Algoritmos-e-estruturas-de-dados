2dd23e43e5bd3fa9015cc067609897c847130475
Eduardo Silva <thiago.novak7@gmail.com>
2021-01-26 17:50:42 -0300

    Add, metodos array

---
+++ Arrays/metodosdearray.js
@@ -0,0 +1,17 @@
+// concat une duas arrays
+const zeero = 0;
+const postiveNumbers = [1, 2, 3];
+const negativeNumbers = [-3, -2, -1];
+let numbers = negativeNumbers.concat(zero, postiveNumbers);
+
+// Funções de interação
+function isEven(x){
+    // devolve true se x for múltiplo de 2.
+    console.log(x);
+    return x % 2 === 0 ? true : false;
+}
+
+
+let numbersOne = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Método every
